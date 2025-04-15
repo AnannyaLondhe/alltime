0fae8c93db514da3adf2138aaa990c503820c218
AnannyaLondhe <141495526+AnannyaLondhe@users.noreply.github.com>
2025-04-15 19:08:52 +0530

    Update sch.py

--- sch.py
+++ sch.py
@@ -1,4 +1,3 @@
-if autosys_schema == 'AML':
-    date_range = get_date_range(from_date_str, to_date_str)
-else:
-    date_range = fn_get_as_of_dt(autosys_schema, sql_main[i][2], from_date_str, to_date_str)
+from_date_str = sys.argv[1].upper()
+to_date_str = sys.argv[2].upper()
+
